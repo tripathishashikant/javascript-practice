52a47d116bc3cac3290c6ebc6c4e86e0e6832afe
Shashikant <shashikant.tripathi187@gmail.com>
2026-03-31 23:17:44 +0530

    Resolved Persistent Loader problem.

---
+++ promise/persistent-loader/index.js
@@ -0,0 +1,35 @@
+// A mock API that fails the first 2 times and succeeds on the 3rd
+let count = 0;
+const unstableAPI = () =>
+  new Promise((res, rej) => {
+    count++;
+
+    if (count < 3) {
+      console.log(`Attempt ${count}: Failed...`);
+
+      rej("Server Error");
+    } else {
+      res("Success! Data received.");
+    }
+  });
+
+function fetchWithRetry(taskFn, retries, delayMs) {
+  return new Promise((resolve, reject) => {
+    const attempt = () => {
+      taskFn()
+        .then(resolve)
+        .catch((e) => {
+          if (count < retries) {
+            setTimeout(attempt, delayMs);
+          } else {
+            reject(e);
+          }
+        });
+    };
+
+    attempt();
+  });
+}
+
+// Expected Usage:
+fetchWithRetry(unstableAPI, 3, 1000).then(console.log).catch(console.error);
